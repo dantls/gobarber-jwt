7bab05f782b6c282159e577fa88c02279fef0afd
dantls <dan_tls@hotmail.com>
2020-02-06 01:14:14 -0400

    funcionality and schedules

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +20,12 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', authMiddleware, UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
